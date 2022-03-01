d03f63b8f267938fe539918c9c4bb69f3d02f5ee
Håkon <haakon8855@users.noreply.github.com>
2022-03-01 20:32:37 +0100

    Removed animation on popup open

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+/**
+ * Haakon8855
+ */
+
 chrome.runtime.onInstalled.addListener(() => {
   let isEnabled = true;
   chrome.storage.sync.set({ isEnabled });
--- popup.js
+++ popup.js
@@ -1,20 +1,34 @@
-let toggleButton = document.getElementById("toggleEnabled");
+/**
+ * Haakon8855
+ */
 
-function updatePopup(isEnabled) {
-    let toggleButton = document.getElementById("toggleEnabled");
-    toggleButton.checked = isEnabled
-    // let flavorText = document.getElementById("textStatus");
-    // flavorText.innerHTML = (isEnabled ? "Enabled" : "Disabled");
+let toggleButton = document.getElementById("toggleEnabled");
+let slider = document.getElementById("sliderElem");
+
+let updateSlider = (isEnabled) => {
+    // Updates the state of the slider button
+    toggleButton.checked = isEnabled;
 }
 
+// Set the slider button to the state stored in storage
 chrome.storage.sync.get("isEnabled", ({ isEnabled }) => {
-    updatePopup(isEnabled);
+    updateSlider(isEnabled);
 });
 
+// Add the transition animation after correct state has been set.
+// For some reason it did not work simply adding the class right away
+// so I added a small delay to avoid seeing the animation upon opening
+// the popup.
+setTimeout(() => {
+    slider.classList.add("sliderAnim");
+}, 100);
+
+// Add the event listener to the slider checkbox in order to store the updated
+// state in storage.
 toggleButton.addEventListener("click", async () => {
     chrome.storage.sync.get("isEnabled", ({ isEnabled }) => {
         isEnabled = !isEnabled;
         chrome.storage.sync.set({ isEnabled });
-        updatePopup(isEnabled);
+        updateSlider(isEnabled);
     });
 });
